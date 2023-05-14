d7418a903a065c97636c1fc59d26fd61421c75a6
David Canales Parra <canales.126497@e.unavarra.es>
2023-05-14 11:06:01 +0000

    [21] - Terminada funcionalidad de crear cartera

--- tests/Infrastructure/Persistence/WalletDataSourceTest.php
+++ tests/Infrastructure/Persistence/WalletDataSourceTest.php
@@ -3,9 +3,45 @@
 namespace Tests\Infrastructure\Persistence;
 
 use App\Infrastructure\Persistence\WalletDataSource;
-use PHPUnit\Framework\TestCase;
+use App\Domain\User;
+use App\Domain\Wallet;
+use Illuminate\Support\Facades\Cache;
+use Tests\TestCase;
+use Mockery;
 
 class WalletDataSourceTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function createsWalletWhenUserExists()
+    {
+        $user = new User(1);
+        Cache::shouldReceive('get')
+            ->with('u_1')
+            ->andReturn($user);
+        Cache::expects('forever');
 
+        $walletDataSource = new WalletDataSource();
+
+        $response = $walletDataSource->createWallet('u_1');
+
+        $this->assertEquals('1_1', $response);
+    }
+
+    /**
+     * @test
+     */
+    public function returnsNullWhenUserDontExist()
+    {
+        Cache::shouldReceive('get')
+            ->with('u_1')
+            ->andReturn(null);
+
+        $walletDataSource = new WalletDataSource();
+
+        $response = $walletDataSource->createWallet('u_1');
+
+        $this->assertNull($response);
+    }
 }
