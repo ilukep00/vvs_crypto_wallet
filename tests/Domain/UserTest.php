d95df9fdeffd3f15d2920dd1520d86c37d871d14
David Canales Parra <104628114+canales126497@users.noreply.github.com>
2023-05-14 11:07:23 +0200

    22 crear modelos user wallet coin (#23)
    * [22] - Primera aproximación del modelo Coin
    
    * [22] - Creados test e implementación para comprar moneda nueva en cartera
    
    * [22] - Creados test e implementación para volver a comprar una moneda
    
    * [22] - Refactorizado
    
    * [22] - Añadida funcionalidad para crear multiples carteras
    
    * [22] - Añadidos test e implementación para maximo numero de creación de carteras

---
+++ tests/Domain/UserTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Domain;
+
+use App\Domain\User;
+use App\Domain\Wallet;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function canCreateMultipleWallets()
+    {
+        $user = new User(1);
+
+        $walletNum = 3;
+        for ($i = 0; $i < $walletNum; $i++) {
+            $user->newWallet();
+        }
+
+        $this->assertEquals($walletNum, $user->getNumOfWallets());
+    }
+
+    /**
+     * @test
+     */
+    public function returnsNullOnCreateWalletIfOver999()
+    {
+        $user = new User(1);
+
+        for ($i = 0; $i < 999; $i++) {
+            $user->newWallet();
+        }
+        $wallet = $user->newWallet();
+
+        $this->assertNull($wallet);
+    }
+}
