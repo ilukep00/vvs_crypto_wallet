58b3532725a4c8e5463c9952fda3089c089549c1
David Canales Parra <canales.126497@e.unavarra.es>
2023-05-14 10:36:27 +0000

    [21] - Modificadas codificaciones de modelos

---
+++ tests/Infrastructure/Persistence/WalletDataSourceTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Infrastructure\Persistence;
+
+use App\Infrastructure\Persistence\WalletDataSource;
+use PHPUnit\Framework\TestCase;
+
+class WalletDataSourceTest extends TestCase
+{
+
+}
