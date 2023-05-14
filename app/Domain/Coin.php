fedd63289ed6f16a4f43755ea68c49570d7b3546
David Canales Parra <104628114+canales126497@users.noreply.github.com>
2023-05-14 11:07:23 +0200

    22 crear modelos user wallet coin (#23)
    * [22] - Primera aproximación del modelo Coin
    
    * [22] - Creados test e implementación para comprar moneda nueva en cartera
    
    * [22] - Creados test e implementación para volver a comprar una moneda
    
    * [22] - Refactorizado
    
    * [22] - Añadida funcionalidad para crear multiples carteras
    
    * [22] - Añadidos test e implementación para maximo numero de creación de carteras

---
+++ app/Domain/Coin.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Domain;
+
+class Coin
+{
+    public string $id;
+    public string $name;
+    public string $symbol;
+    public float $ammount;
+    public float $invertedMoney;
+
+    public function __construct(
+        string $id = '',
+        string $name = '',
+        string $symbol = '',
+        float $ammount = 0,
+        float $invertedMoney = 0
+    ) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->symbol = $symbol;
+        $this->ammount = $ammount;
+        $this->invertedMoney = $invertedMoney;
+    }
+
+    public function add(Coin $coinToAdd): Coin
+    {
+        $this->ammount = $this->ammount + $coinToAdd->ammount;
+        $this->invertedMoney = $this->invertedMoney + $coinToAdd->invertedMoney;
+
+        return $this;
+    }
+}
