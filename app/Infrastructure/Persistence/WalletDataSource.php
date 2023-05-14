9f5197f8d7d71b9f91eccfa50ad53dda16c7858e
David Canales Parra <canales.126497@e.unavarra.es>
2023-05-14 11:12:41 +0000

    [21] - Terminada funcionalidad de crear cartera

--- app/Infrastructure/Persistence/WalletDataSource.php
+++ app/Infrastructure/Persistence/WalletDataSource.php
@@ -2,11 +2,27 @@
 
 namespace App\Infrastructure\Persistence;
 
+use Illuminate\Support\Facades\Cache;
+use App\Domain\Wallet;
+
 class WalletDataSource
 {
     public function createWallet(string $userId): string|null
     {
-        // TODO
-        return null;
+        if (is_null(Cache::get($userId))) {
+            return null;
+        }
+
+        $user = Cache::get($userId);
+
+        $newWallet = $user->newWallet();
+
+        if ($newWallet == null) {
+            return null;
+        }
+
+        Cache::forever("w_" . $newWallet->getId(), $newWallet);
+
+        return $newWallet->getId();
     }
 }
