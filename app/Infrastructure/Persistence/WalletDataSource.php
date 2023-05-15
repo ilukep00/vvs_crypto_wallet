6f8405ebbe7817bde1a3a6178c73dc18641698dc
David Canales Parra <104628114+canales126497@users.noreply.github.com>
2023-05-15 13:10:04 +0200

    4 crear una cartera nueva (#35)
    * [16] - Añadida respuesta a peticion invalida
    
    * [16] - Refactorizados implementacion y test
    
    * [16] - Solucionado fallo en peticion del test
    
    * [14] - Creados test e implementación para gestionar el error de user not found
    
    * [21] - Modificado return value a string o null
    
    * [17] - Creados test e implementacion para devolucion de id en caso de creacion y adaptacion de tests anteriores
    
    * [4] - Cambiados asserts de estado a assertStatus(codigo)
    
    * 22 crear modelos user wallet coin (#23)
    
    * [22] - Primera aproximación del modelo Coin
    
    * [22] - Creados test e implementación para comprar moneda nueva en cartera
    
    * [22] - Creados test e implementación para volver a comprar una moneda
    
    * [22] - Refactorizado
    
    * [22] - Añadida funcionalidad para crear multiples carteras
    
    * [22] - Añadidos test e implementación para maximo numero de creación de carteras
    
    * [21] - Modificadas codificaciones de modelos
    
    * [21] - Terminada funcionalidad de crear cartera
    
    * [21] - Terminada funcionalidad de crear cartera
    
    * [4] - Creado binding para WalletDataSource sobre la ruta
    
    * [4] - Modificado para uso de UserDataSource
    
    * [4] - Cambiada nomenclatura de almacenamiento de los datos en cache

---
+++ app/Infrastructure/Persistence/WalletDataSource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Infrastructure\Persistence;
+
+use Illuminate\Support\Facades\Cache;
+use App\Domain\Wallet;
+
+class WalletDataSource
+{
+    private UserDataSource $userDataSource;
+
+    public function __construct()
+    {
+        $this->userDataSource = new UserDataSource();
+    }
+
+    public function createWallet(string $userId): string|null
+    {
+        if (is_null(Cache::get("user_" . $userId))) {
+            return null;
+        }
+
+        $user = Cache::get("user_" . $userId);
+        $newWallet = $user->newWallet();
+        $this->userDataSource->save($user);
+
+        if ($newWallet == null) {
+            return null;
+        }
+
+        Cache::forever("wallet_" . $newWallet->getId(), $newWallet);
+
+        return $newWallet->getId();
+    }
+}
